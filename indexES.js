8c859475c6dceae2f1cec0fdaa31d124e0230970
Ruchi Ray <ruchiray18@gmail.com>
2021-03-17 21:02:41 +0530

    storage system added

--- indexES.js
+++ indexES.js
@@ -1,25 +1,45 @@
 // this is es6 version of project
+
 console.log('hihi')
 class Book {
     constructor(name, author, genre) {
         this.name = name;
         this.author = author;
         this.genre = genre;
+        
     }
 }
-class Display{
-    add(book, count) {
+class Display {
+    add() {
         console.log('adding to ui')
+        let item = localStorage.getItem('items')
+        let itemObj
+        if (item == null) {
+            itemObj = []
+        }
+        else {
+            itemObj = JSON.parse(item)
+        }
+        let uiString = ''
         let tableBoddy = document.getElementById('tableBody')
-        let uiString = ` <tr>
-                            <th scope="row">${count}</th>
-                            <td>${book.name}</td>
-                            <td>${book.author}</td>
-                            <td>${book.genre}</td>
-                         </tr>
-                           `
-        tableBoddy.innerHTML += uiString
+        itemObj.forEach(function (element,index) {
+            uiString += `<tr>
+                                <th scope="row">${index+1}</th>
+                                <td>${element.name}</td>
+                                <td>${element.author}</td>
+                                <td>${element.genre}</td>
+                                <td> <button type="submit" class="btn btn-primary bg-blue effect pad" id = "${index}" onclick = "deleteBook(this.id)">Delete</button></td>
+                             </tr>
+                               `
 
+        });
+        if (itemObj.length != 0) {
+            tableBoddy.innerHTML = uiString
+        }
+        else {
+           tableBoddy.innerHTML = '<h3 class = "blueText">No book added !!</h3>'
+        }
+       
     }
     clear() {
         let libraryform = document.getElementById('libraryForm');
@@ -33,24 +53,24 @@ class Display{
             return true;
         }
     }
-    show(type,message){
+    show(type, message) {
         let msg = document.getElementById('msg')
         msg.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
                            <strong>Holy guacamole!</strong> ${message}
                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                            </div>`;
-       setTimeout(function(){
-           msg.innerHTML = ''
-       },8000)
-       }
+        setTimeout(function () {
+            msg.innerHTML = ''
+        }, 8000)
+    }
 }
+
 // add submit event listener to libraryform
 let libraryform = document.getElementById('libraryForm');
 libraryform.addEventListener('submit', libraryFormSubmit)
-let count = 0;
 function libraryFormSubmit(e) {
     e.preventDefault();
-    count = count+1;
+   
     console.log('you have submitted library form')
     let name = document.getElementById('bookName').value;
     let author = document.getElementById('author').value;
@@ -68,15 +88,50 @@ function libraryFormSubmit(e) {
         genre = ref.value
     }
     let book = new Book(name, author, genre)
-    let display = new Display()
-    if(display.validate(book)){
-        display.add(book,count)
-        display.clear();
-        display.show('success','sucess')
+    // let myobj = {
+    //     bookName:name,
+    //     authorName:author,
+    //     genreName:genre,
+    //     sno:count
+    // }
+    console.log(book)
+    let item = localStorage.getItem('items')
+    let itemObj
+    if (item == null) {
+        itemObj = []
     }
-    else{
-        display.show('danger','error')
+    else {
+        itemObj = JSON.parse(item)
+    }
+    let display = new Display()
+    if (display.validate(book)) {
+        itemObj.push(book);
+        localStorage.setItem('items', JSON.stringify(itemObj))
+        display.add()
+        display.clear();
+        display.show('success', 'sucess')
+    }
+    else {
+        display.show('danger', 'error')
         // show error
     }
-   
+
 }
+// deleting book
+function  deleteBook(params) {
+    let item = localStorage.getItem('items')
+    let itemObj
+    if (item == null) {
+        itemObj = [];
+    }
+    else {
+        itemObj = JSON.parse(item)
+    }
+    itemObj.splice(params,1)
+    
+    localStorage.setItem('items', JSON.stringify(itemObj))
+    let disp = new Display()
+    disp.add();
+}
+let dis = new Display()
+dis.add();
